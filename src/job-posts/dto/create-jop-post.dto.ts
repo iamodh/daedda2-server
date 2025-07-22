e6ae08ab1ca8adf449a4e246b4cbbe6840fc351e
Donghwan Oh <imodh97@gmail.com>
2025-07-22 22:06:41 +0900

    테스트 코드 제거

--- src/job-posts/dto/create-jop-post.dto.ts
+++ src/job-posts/dto/create-jop-post.dto.ts
@@ -7,59 +7,7 @@ import {
   Matches,
   MaxLength,
   MinLength,
-  //   ValidationArguments,
-  //   ValidatorConstraint,
-  //   ValidatorConstraintInterface,
 } from 'class-validator';
-
-// @ValidatorConstraint({ name: 'isStartBeforeEnd', async: false })
-// export class isStartBeforeEnd implements ValidatorConstraintInterface {
-//   private reason: 'inavlid-format' | 'not-before' | null = null;
-
-//   validate(_: any, args: ValidationArguments): boolean {
-//     const obj = args.object as any; // 전체 DTO
-//     const startTime: string = obj.startTime;
-//     const endTime: string = obj.endTime;
-
-//     if (!startTime || !endTime) {
-//       this.reason = null;
-//       return true;
-//     } // 다른 validator가 검사
-
-//     const [startHour, startMin] = startTime.split(':').map(Number);
-//     const [endHour, endMin] = endTime.split(':').map(Number);
-
-//     // 검사 1. HH:MM 형태인가?
-//     if (
-//       isNaN(startHour) ||
-//       isNaN(startMin) ||
-//       isNaN(endHour) ||
-//       isNaN(endMin)
-//     ) {
-//       this.reason = 'inavlid-format';
-//       return false;
-//     }
-
-//     const startMinutes = startHour * 60 + startMin;
-//     const endMinutes = endHour * 60 + startMin;
-
-//     // 검사 2. 시작 시간이 마치는 시간보다 작은가?
-//     if (startMinutes >= endMinutes) {
-//       this.reason = 'not-before';
-//     }
-
-//     this.reason = null;
-//     return true;
-//   }
-
-//   defaultMessage() {
-//     if (this.reason === 'inavlid-format') {
-//       return '시간 형식이 잘못되었습니다. (예: 16:30)';
-//     } else if (this.reason === 'not-before')
-//       return 'startTime은 endTime보다 빠른 시간이어야 합니다.';
-//     return '시간이 유효하지 않습니다.'; // fallback
-//   }
-// }
 
 export class CreateJobPostDto {
   @IsString()
