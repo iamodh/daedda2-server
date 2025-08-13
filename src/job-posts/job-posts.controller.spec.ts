89da3761a7859f0fea51f8ec12ead178844a6adc
Donghwan Oh <imodh97@gmail.com>
2025-08-13 15:04:35 +0900

    테스트 이름 수정

--- src/job-posts/job-posts.controller.spec.ts
+++ src/job-posts/job-posts.controller.spec.ts
@@ -2,7 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { JobPostsController } from './job-posts.controller';
 import { AuthGuard } from '../auth/auth.guard';
 import { JobPostsService } from './job-posts.service';
-import { DeleteResult, InsertResult } from 'typeorm';
+import { DeleteResult, InsertResult, UpdateResult } from 'typeorm';
 import { CreateJobPostDto } from '../job-posts/dto/create-job-post.dto';
 import { JobPost } from '../job-posts/entities/job-post.entity';
 import { NotFoundException } from '@nestjs/common';
@@ -11,24 +11,30 @@ import { UpdateJobPostDto } from '../job-posts/dto/update-job-post.dto';
 
 describe('JobPostsController', () => {
   let controller: JobPostsController;
-
-  const mockJobPostsService = {
-    findAll: jest.fn(),
-    create: jest.fn(),
-    findOne: jest.fn(),
-    update: jest.fn(),
-    delete: jest.fn(),
-  };
+  let jobPostsService: jest.Mocked<
+    Pick<
+      JobPostsService,
+      'create' | 'delete' | 'findAll' | 'findOne' | 'update'
+    >
+  >;
 
   beforeEach(async () => {
     jest.clearAllMocks();
+
+    jobPostsService = {
+      findAll: jest.fn(),
+      create: jest.fn(),
+      findOne: jest.fn(),
+      update: jest.fn(),
+      delete: jest.fn(),
+    };
 
     const module: TestingModule = await Test.createTestingModule({
       controllers: [JobPostsController],
       providers: [
         {
           provide: JobPostsService,
-          useValue: mockJobPostsService,
+          useValue: jobPostsService,
         },
       ],
     })
@@ -44,40 +50,40 @@ describe('JobPostsController', () => {
   });
 
   describe('findAll', () => {
-    it('유효한 dto가 주어지면 dto를 서비스에 그대로 전달하고 반환값을 그대로 응답한다.', async () => {
+    it('dto를 서비스에 전달하고 반환값을 그대로 응답한다.', async () => {
       const dto = { limit: 5, cursor: null };
-      const result = { data: [{ id: 1 }], nextCursor: 'abc' };
-      mockJobPostsService.findAll.mockResolvedValue(result);
+      const result = { data: [{ id: 1 } as JobPost], nextCursor: 'abc' };
 
+      jobPostsService.findAll.mockResolvedValue(result);
       await expect(controller.findAll(dto)).resolves.toEqual(result);
-      expect(mockJobPostsService.findAll).toHaveBeenCalledWith(dto);
+      expect(jobPostsService.findAll).toHaveBeenCalledWith(dto);
     });
   });
 
   describe('create', () => {
-    it('요청 dto를 서비스에 전달하고 InsertResult(identifiers 포함)를 그대로 응답한다', async () => {
+    it('dto를 서비스에 전달하고 반환값을 그대로 응답한다.', async () => {
       const dto = { title: 't' } as CreateJobPostDto;
-      const result: Partial<InsertResult> = {
+      const result = {
         identifiers: [{ id: 1 }],
-      };
-      mockJobPostsService.create.mockResolvedValue(result);
+      } as unknown as InsertResult;
+      jobPostsService.create.mockResolvedValue(result);
 
       await expect(controller.create(dto)).resolves.toEqual(result);
-      expect(mockJobPostsService.create).toHaveBeenCalledWith(dto);
+      expect(jobPostsService.create).toHaveBeenCalledWith(dto);
     });
   });
 
   describe('findOne', () => {
     it('id를 서비스에 전달하고 해당 JobPost를 그대로 응답한다', async () => {
       const post = { id: 123 } as JobPost;
-      mockJobPostsService.findOne.mockResolvedValue(post);
+      jobPostsService.findOne.mockResolvedValue(post);
 
       await expect(controller.findOne(123)).resolves.toEqual(post);
-      expect(mockJobPostsService.findOne).toHaveBeenCalledWith(123);
+      expect(jobPostsService.findOne).toHaveBeenCalledWith(123);
     });
 
-    it('jobPostId와 req.user.sub를 ownerId로 매핑해 서비스에 전달하고 UpdateResult를 그대로 응답한다', async () => {
-      mockJobPostsService.findOne.mockRejectedValue(new NotFoundException());
+    it('서비스에서 반환한 에러를 그대로 응답한다', async () => {
+      jobPostsService.findOne.mockRejectedValue(new NotFoundException());
 
       await expect(controller.findOne(999)).rejects.toBeInstanceOf(
         NotFoundException,
@@ -86,27 +92,27 @@ describe('JobPostsController', () => {
   });
 
   describe('update', () => {
-    it('jobPostId와 req.user.sub를 ownerId로 매핑해 서비스에 전달하고 DeleteResult를 그대로 응답한다', async () => {
+    it('jobPostId, req user id와 함께 dto를 서비스에 전달하고 반환값을 그대로 응답한다', async () => {
       const dto = { title: 'u' } as UpdateJobPostDto;
       const req = { user: { sub: 42 } } as AuthRequest;
-      const result: Partial<DeleteResult> = { affected: 1 };
+      const result = { affected: 1 } as UpdateResult;
 
-      mockJobPostsService.update.mockResolvedValue(result);
+      jobPostsService.update.mockResolvedValue(result);
 
       await expect(controller.update(7, dto, req)).resolves.toEqual(result);
-      expect(mockJobPostsService.update).toHaveBeenCalledWith(7, 42, dto);
+      expect(jobPostsService.update).toHaveBeenCalledWith(7, 42, dto);
     });
   });
 
   describe('delete', () => {
-    it('jobPostId와 ownerId를 delete 서비스에 전달해야 한다.', async () => {
+    it('jobPostId, req user id를 서비스에 전달하고 반환값을 그대로 응답한다.', async () => {
       const req = { user: { sub: 42 } } as AuthRequest;
-      const result: Partial<DeleteResult> = { affected: 1 };
+      const result = { affected: 1 } as DeleteResult;
 
-      mockJobPostsService.delete.mockResolvedValue(result);
+      jobPostsService.delete.mockResolvedValue(result);
 
       await expect(controller.delete(7, req)).resolves.toEqual(result);
-      expect(mockJobPostsService.delete).toHaveBeenCalledWith(7, 42);
+      expect(jobPostsService.delete).toHaveBeenCalledWith(7, 42);
     });
   });
 });
