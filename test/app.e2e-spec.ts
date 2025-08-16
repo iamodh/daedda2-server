69c54b4510e5d268ca0d4c18486e34549ae30ef0
Donghwan Oh <imodh97@gmail.com>
2025-08-16 15:40:09 +0900

    e2e 테스트 todo 작성

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -4,6 +4,10 @@ import * as request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
 
+// 테스트 db 생성
+// boforeall에서 app init, 서비스 연결, db 연결
+// boforeEach에서 db 초기화
+
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
 
