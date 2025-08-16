ff5de5b4e0eb1d5a2eb1260655e20d219631b299
Donghwan Oh <imodh97@gmail.com>
2025-08-16 15:40:36 +0900

    app module에서 config service 분리

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,27 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { DataSource } from 'typeorm';
 import { JobPostsModule } from './job-posts/job-posts.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmConfigService } from './config/typeorm.config.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        type: 'postgres',
-        host: config.get<string>('DB_HOST'),
-        port: config.get<number>('DB_PORT'),
-        username: config.get<string>('DB_USERNAME'),
-        password: config.get<string>('DB_PASSWORD'),
-        database: config.get<string>('DB_NAME'),
-        synchronize: true,
-        autoLoadEntities: true,
-      }),
+      useClass: TypeOrmConfigService,
     }),
     JobPostsModule,
     AuthModule,
